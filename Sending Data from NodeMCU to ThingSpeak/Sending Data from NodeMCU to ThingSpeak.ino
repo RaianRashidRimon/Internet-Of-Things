e1e220f2cb1e732b250e1c889024e9f3a4a4857e
Raian Rashid <rimonraian2@gmail.com>
2025-10-22 00:46:13 +0600

    exp 7 code

--- Sending Data from NodeMCU to ThingSpeak/Sending Data from NodeMCU to ThingSpeak.ino
+++ Sending Data from NodeMCU to ThingSpeak/Sending Data from NodeMCU to ThingSpeak.ino
@@ -13,33 +13,26 @@ const char* myWriteAPIKey = "KYLK0JE01RRY5R2O";
 void setup() {
     Serial.begin(9600);
     delay(10);
-
     WiFi.begin(ssid, password);
     Serial.print("Connecting to WiFi");
-    
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
     Serial.println("\nConnected to WiFi");
-
     ThingSpeak.begin(client);
 }
-
 void loop() {
-    val = (analogRead(pin) * 3.3 / 1024.0) * 100; // Convert ADC reading to Celsius
+    val = (analogRead(pin) * 3.3 / 1024.0) * 100; 
     Serial.print("Temperature: ");
     Serial.print(val);
     Serial.println(" *C");
-
     int response = ThingSpeak.writeField(myChannelNumber, 1, val, myWriteAPIKey);
-
     if (response == 200) {
         Serial.println("Data sent to ThingSpeak successfully!");
     } else {
         Serial.print("Error sending data. HTTP Code: ");
         Serial.println(response);
     }
-
-    delay(15000); // 15 seconds delay for ThingSpeak
+    delay(15000);
 }
